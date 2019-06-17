4740c8f4db2d2ce7e550e966c415909648aec5e0
s14113135 <s14113135@stu.edu.tw>
2019-06-17 15:46:31 +0800

    請求參數、標頭

---
+++ src/cc/openhome/Headers.java
@@ -0,0 +1,36 @@
+package cc.openhome;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.*;
+import java.io.*;
+
+@WebServlet("/headers")
+public class Headers extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PrintWriter out = response.getWriter();
+        out.println("<!DOCTYPE html>");
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<title>Servlet ShowHeader</title>");
+        out.println("</head>");
+        out.println("<body>");
+
+        Collections.list(request.getHeaderNames())
+                .forEach(name -> {
+                    out.printf("%s: %s<br>%n", name, request.getHeader(name));
+                });
+
+        out.println("</body>");
+        out.println("</html>");
+        out.close();
+    }
+}
